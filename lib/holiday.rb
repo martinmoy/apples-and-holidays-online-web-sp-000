f9015adde8c6c8debfa622592165f4f3cd0afc7a
Martin Moy <martinmoy1991@gmail.com>
2019-09-12 15:41:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, items|
       if items.include?("BBQ")
-        holiday.compact
+        holiday.compact.flatten
       end
     end
   end
