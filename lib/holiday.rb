131d7a635010682550dfb9c8910cfcacaf6a961b
Martin Moy <martinmoy1991@gmail.com>
2019-09-12 15:49:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,15 +69,12 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, items|
       if items.include?("BBQ")
-        holiday = holiday.to_s.split.compact
+        all_holidays_with_bbq << holiday
       end
     end
   end
-
-
+  holidays_with_BBQ.uniq
 end
